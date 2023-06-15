140ffb7a2407d01a5e98a74467ad95e2fb909c1f
Noah Gift <noah.gift@gmail.com>
2023-06-15 15:38:00 +0000

    garbage

--- print-data-structs/target/debug/deps/print_data_structs-b0c5eb920f6d9265.d
+++
@@ -1,8 +0,0 @@
-/workspaces/rust-data-engineering/print-data-structs/target/debug/deps/print_data_structs-b0c5eb920f6d9265.rmeta: src/main.rs Cargo.toml
-
-/workspaces/rust-data-engineering/print-data-structs/target/debug/deps/print_data_structs-b0c5eb920f6d9265.d: src/main.rs Cargo.toml
-
-src/main.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
--- print-data-structs/target/debug/print-data-structs.d
+++
@@ -1,1 +0,0 @@
-/workspaces/rust-data-engineering/print-data-structs/target/debug/print-data-structs: /workspaces/rust-data-engineering/print-data-structs/src/main.rs
